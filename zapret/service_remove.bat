cfb72f239c00dfb778e63a9b6bfc7ecbdeef8316
Aiken <124787991+AikenOZ@users.noreply.github.com>
2024-10-10 03:49:48 -0400

    Zapret v1.1.1

---
+++ zapret/service_remove.bat
@@ -0,0 +1,25 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+:: Path check
+set scriptPath=%~dp0
+set "path_no_spaces=%scriptPath: =%"
+if not "%scriptPath%"=="%path_no_spaces%" (
+    echo Путь содержит пробелы. 
+    echo Пожалуйста, переместите скрипт в директорию без пробелов.
+    pause
+    exit /b
+)
+
+:: Admin rights check
+echo Данный файл должен быть запущен с правами администратора (ПКМ -> Запустить от имени администратора).
+echo Нажмите любую клавишу, чтобы продолжить удаление и остановку сервиса.
+pause
+
+set SRVCNAME=zapret
+
+net stop "%SRVCNAME%"
+sc delete "%SRVCNAME%"
+
+pause
